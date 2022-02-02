1f27333edcc7ccfc347067c60519a16404a79c5d
jacobhaskins <jacob.haskins@gmail.com>
2022-02-02 14:55:51 -0500

    Add baseUrl to Cypress config

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,6 +1,6 @@
 describe('Stock Comparison Integration Test', () => {
   it('successfully loads', () => {
-    cy.visit('http://localhost:3000')
+    cy.visit('/')
   })
 
   it('should render', () => {
